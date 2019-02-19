d33951768f934408af45299edc9d793dab05deaf
liangjie <330556037@qq.com>
2019-02-19 19:36:30 +0800

    add_login_page

--- login.js
+++ login.js
@@ -1,8 +1,26 @@
-const {app,BrowserWindow} = require("electron");
+const {app,BrowserWindow,Tray,Menu} = require('electron');
+const nativeImage = require('electron').nativeImage
+const path = require('path');
+
+let tray = null
+
 function createWindow(){
-    win = new  BrowserWindow({width:480,height:304});
-    win.loadFile("login.html");
-    win.webContents.openDevTools();
+    win = new  BrowserWindow({width:365,height:660,show:false,icon:"image/icon.ico"});
+    //,kiosk:false,maximizable:false,maxWidth:365,maxHeight:660,minWidth:365,minHeight:660
+    win.loadFile("html/login.html");
+    win.on("ready-to-show",()=>{
+        win.show();
+    })
+    let image = nativeImage.createFromPath("image/icon.ico");
+    tray = new Tray(image);
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item2', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' }
+    ])
+    tray.setToolTip('This is my application.');
+    tray.setContextMenu(contextMenu);
     win.on('closed',()=>{
         console.log('closed');
         win = null;
